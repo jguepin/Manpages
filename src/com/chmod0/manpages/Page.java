8aff336318ae8bde10e5ade000b70fe2059661f0
Julien Guépin <julien@guepin.fr>
2012-02-29 00:00:18 +0100

    First commit

---
+++ src/com/chmod0/manpages/Page.java
@@ -0,0 +1,41 @@
+package com.chmod0.manpages;
+
+import java.io.Serializable;
+
+public class Page implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private int section;
+	
+	public Page(String name, int section) {
+		super();
+		this.name = name;
+		this.section = section;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSection() {
+		return section;
+	}
+
+	public void setSection(int section) {
+		this.section = section;
+	}
+	
+	@Override
+	public String toString(){
+		return this.name + " (" + this.section + ")";
+	}
+
+}
